18737bde4a4d08d401a650c094bd06f1afecf22a
Arup Kumar Behera <76170236+ArupGhoost@users.noreply.github.com>
2021-04-14 00:08:57 +0530

    Add files via upload

---
+++ PSticky.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import './PSticky.scss'
+
+const PSticky = () => {
+    return (
+        <>
+
+            <div className='main_photo'></div>
+            <div className='header'>Justice League Snyder's Cut(2021)</div>
+            <div style={{backgroundColor:'white'}}>
+            <br /><br /><br /><br /><br /><br /></div>
+            <div className='navbar'>
+                <a href='#'>Home</a>
+                <a href='$'>Log In</a>
+            </div>
+            <div className='para'>
+                <p>Thousands of years ago, Darkseid and his</p>
+                 <p>Parademons attempted to
+                 conquer Earth using</p> 
+                 <p>the Mother Boxes after</p>
+                 <p>learning that Earth harbored the Anti-Life Equation</p>
+                 <p>The attempt was foiled by a unified alliance of Old Gods,</p>
+                  <p>Amazons, Atlanteans, humanity, and a Green Lantern. After the battle,</p>
+                  <p>the Boxes were hidden in different locations,</p> 
+                  <p>guarded by the Amazons, Atlanteans, and Humanity, respectively</p>
+                  <p>In the present, Superman's death triggers the Boxes'</p>
+                   <p>reactivation, attracting Steppenwolf,</p> 
+                  <p>Darkseid's disgraced lieutenant, to Earth. Steppenwolf aims to regain</p>
+                   <p>Darkseid's favor by gathering the boxes to form "the Unity,"</p> 
+                  <p>which would terraform Earth in the likeness of their world Apokolips.</p>
+                  <p>Steppenwolf retrieves a Mother Box from Themyscira,</p>
+                  <p>prompting Queen Hippolyta to warn</p> 
+                  <p>her daughter Diana Prince. Diana receives the</p> 
+                  <p>message and learns of the events</p> 
+                  <p>related to Darkseid and Steppenwolf. She informs Bruce Wayne.</p> 
+                  <p>Bruce and Diana seek to form a team of metahumans to protect Earth.</p>
+                  <p>Bruce locates Arthur Curry and Barry Allen,</p> 
+                 <p> while Diana locates Victor Stone.</p>
+                  <p> Barry joins immediately, while Arthur and Victor refuse. However, Victor</p> 
+                   <p>joins after his father Silas and several other S.T.A.R. Labs</p> 
+                  <p>employees are kidnapped by Parademons seeking</p>
+                   <p> the humans' Mother Box. Steppenwolf attacks</p>
+                   <p>an Atlantean outpost to retrieve their Box,</p> 
+                   <p>forcing Arthur into action.</p>
+                   <p>The team receives intel from Gotham City Police Commissioner James Gordon, </p>
+                   <p>leading them to Steppenwolf's army in an abandoned facility</p> 
+                   <p>under Gotham Harbor. Although the group rescues the kidnapped employees,</p> 
+                   <p>the facility is flooded during combat, trapping</p>
+                   <p> the team until Arthur</p> 
+                   <p>helps delay the flood so they can escape. Victor retrieves</p> 
+                   <p>the last Mother Box,which he had hidden,</p> 
+                   <p>for the group to analyze</p>
+                  <p>Victor reveals that Silas used the Box to rebuild his body</p>
+                   <p>after a car accident and that the Boxes are "change machines"</p> 
+                   <p>that unfalteringly enforce the will of their master, destroying</p>
+                    <p>or restoring to life without bias. The group decides to use the</p>
+                     <p>Box to resurrect Superman to help fight Steppenwolf. Barry and </p>
+                     <p> exhume Superman's body and place it in the amniotic fluid of the</p> 
+                  <p>genesis chamber in a Kryptonian ship[c] kept in S.T.A.R. Labs</p>
+                  <p>After Barry activates the Mother Box, an amnesiac Superman is resurrected,</p>
+                   <p>immediately attacking the group after Victor's cybernetics target him in</p> 
+                   <p>self-defense. Lois Lane arrives just in time to calm Superman,</p>
+                  
+            </div>
+            <div className='more'>
+               <a href=''>Read More</a>
+            </div>
+          
+        </>
+    )
+}
+
+export default PSticky;
